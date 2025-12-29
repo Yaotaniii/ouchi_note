6043c73f5023556bdb3a0896047e2acb9edd27c3
yaotani <yaomusic0413@gmail.com>
2025-12-29 20:11:05 +0900

    Add dashboard with authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  # ヘルスチェック用（そのまま残す）
+  get "dashboard", to: "dashboard#index"
+  root "dashboard#index"
+
   get "up" => "rails/health#show", as: :rails_health_check
 end
