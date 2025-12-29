1c4b11fbed36aab8a36ddda7e8a9012a49b56d81
yaotani <yaomusic0413@gmail.com>
2025-12-30 02:19:23 +0900

    Add dependent destroy to models for proper cascade deletion

--- app/models/resident.rb
+++ app/models/resident.rb
@@ -1,21 +1,18 @@
 class Resident < ApplicationRecord
   belongs_to :room
 
-  has_one :contract
-  has_many :vehicles
-  has_many :bicycle_registrations
-  has_many :motorcycle_registrations
-  has_many :payments
-  has_many :incidents
+  has_one :contract, dependent: :destroy
+  has_many :vehicles, dependent: :destroy
+  has_many :payments, dependent: :destroy
+  has_many :incidents, dependent: :destroy
+  has_many :bicycle_registrations, dependent: :destroy
+  has_many :motorcycle_registrations, dependent: :destroy
 
   validates :name, presence: true
   validates :move_in_date, presence: true
+  validates :occupants_count, numericality: { greater_than_or_equal_to: 1 }, allow_nil: true
 
   def current?
     move_out_date.nil?
-  end
-
-  def moved_out?
-    move_out_date.present?
   end
 end
