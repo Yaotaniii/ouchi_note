cb7e19ff2bfb898d8501d064a9fe9170045f676e
yaotani <yaomusic0413@gmail.com>
2025-12-29 17:43:46 +0900

    Configure Devise and test user login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,4 @@
 class User < ApplicationRecord
-  has_secure_password
-
-  validates :email, presence: true, uniqueness: true
-  validates :name, presence: true
-  validates :role, presence: true, inclusion: { in: %w[owner staff] }
-
-  def owner?
-    role == 'owner'
-  end
-
-  def staff?
-    role == 'staff'
-  end
-end
-
-class User < ApplicationRecord
   devise :database_authenticatable, :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
